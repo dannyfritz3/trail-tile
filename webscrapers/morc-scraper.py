3c99a554e1cd7e5de37b52b7fceb33e03facb4e0
Danny Fritz <DRFRITZ@csbsju.edu>
2019-11-14 15:38:04 -0600

    Created scraper file for morc website

--- webscrapers/morc-scraper.py
+++ webscrapers/morc-scraper.py
@@ -1,14 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+import json
 
 def main():
-    results = scrape()
+    trail_data = scrape_for_trail_data()
+    print(json.dumps(trail_data))
 
-def scrape():
+def scrape_for_trail_data():
     page = requests.get("http://www.morcmtb.org/forums/trailconditions.php")
     soup = BeautifulSoup(page.content, 'html.parser')
-    test = soup.find_all('table', class_="forumbits")
-    print('test')
+
+    contents = soup.contents
+    raw_trail_info = []
+    parsed_trail_info = []
+
+    for item in contents:
+        if not isinstance(item, str):
+            if item.text.endswith('\r\n\n\n'):
+                raw_trail_info.append(item.text)
+
+    for item in raw_trail_info:
+        parsed_data_str = re.sub(r"((\r)*(\n)+(\t)*(\r)*(\n)*(\t)*)", ',', item)
+        parsed_data_obj = parsed_data_str[1:len(parsed_data_str)-1].split(',')
+        parsed_trail_info.append(parsed_data_obj)
+
+    return parsed_trail_info
 
 if __name__ == "__main__":
     main()
