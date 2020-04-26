7bc457332a12f2f4096430217ae67faa1dab751f
Danny <drfritz@csbsju.edu>
2020-04-25 20:33:47 -0500

    parseWindDirection function

--- src/components/TrailTile/TrailWeather/TrailWeatherOtherData.js
+++ src/components/TrailTile/TrailWeather/TrailWeatherOtherData.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {parseWindDirection} from '../../../utils';
 
 class TrailWeatherOtherData extends React.Component {
     constructor(props) {
@@ -26,23 +27,23 @@ class TrailWeatherOtherData extends React.Component {
                 temp: nextProps.liveWeatherData.temp ? parseInt(nextProps.liveWeatherData.temp.value) : 0,
                 precipitation: nextProps.liveWeatherData.precipitation ? parseInt(nextProps.liveWeatherData.precipitation.value) :0,
                 wind_speed: nextProps.liveWeatherData.wind_speed ? parseInt(nextProps.liveWeatherData.wind_speed.value) : 0,
-                wind_direction: nextProps.liveWeatherData.wind_direction ? nextProps.liveWeatherData.wind_direction.value : ""
+                wind_direction: nextProps.liveWeatherData.wind_direction ? parseWindDirection(nextProps.liveWeatherData.wind_direction.value) : ""
         }})
     }
     render(props) {
         return (
-            <div style={{display: 'inline-block', float: 'right', height: '100%', width: '80px'}}>
+            <div style={{display: 'inline-block', float: 'right', height: '100%', width: '90px'}}>
                 <div className="weather-other-data-block" style={{ display: 'block', height: '33px' }}>
                     <img style={this.state.style} src={require("../../../resource/Icons/WeatherIcons/icons8-thermometer-50.png")} alt="wind" />
-                    <div style={{ display: "inline", fontSize: "12px"}}>{this.state.liveWeatherData.temp} <b>&#8457;</b></div>
+                    <div style={{ display: "inline", fontSize: "11px"}}>{this.state.liveWeatherData.temp} <b>&#8457;</b></div>
                 </div>
                 <div className="weather-other-data-block" style={{display: 'block', height: '33px'}}>
                     <img style={this.state.style} src={require("../../../resource/Icons/WeatherIcons/icons8-wet-50.png")} alt="wind" />
-                    <div style={{display: "inline", fontSize: "12px"}}>{this.state.liveWeatherData.precipitation} <b>in/hr</b></div>
+                    <div style={{display: "inline", fontSize: "11px"}}>{this.state.liveWeatherData.precipitation} <b>in/hr</b></div>
                 </div>
                 <div className="weather-other-data-block" style={{display: 'block', height: '33px'}}>
                     <img style={this.state.style} src={require("../../../resource/Icons/WeatherIcons/icons8-windsock-50.png")} alt="wind" />
-                    <div style={{display: "inline", fontSize: "12px"}}>{this.state.liveWeatherData.wind_speed} <b>mph {this.state.liveWeatherData.wind_direction}</b></div>
+                    <div style={{display: "inline", fontSize: "11px"}}>{this.state.liveWeatherData.wind_speed} <b>mph {this.state.liveWeatherData.wind_direction}</b></div>
                 </div>
             </div>
         );
