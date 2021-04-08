5e362182f0f168bb92d79e9fde9f1d12154d1f88
Danny Fritz <drfritz@csbsju.edu>
2021-04-08 16:50:17 -0500

    this should make the trail titles look a lot better :)

--- src/components/TrailTile/TrailTitle.js
+++ src/components/TrailTile/TrailTitle.js
@@ -7,11 +7,24 @@ class TrailTitle extends React.Component {
             this.props.changeMapHandler(e, reimtbX, reimtbY);
         };
         
-        console.log(this.props.trailName);
-        
+        const resolveAnyLongTrailNames = (trailName) => {
+            switch(trailName) {
+                case "Cottage Grove Bike Park":
+                    return "Cottage Grove"
+                case "Bertram Chain of Lakes":
+                    return "Chain of Lakes";
+                case "Minnesota River Trail":
+                    return "Minnesota River";
+                case "Monarch Singletrack":
+                    return "Monarch";
+                default:
+                    return trailName;
+            }
+        } 
+
         return (
             <div className="info-div">
-                <h2 className="trail-heading"><b>{this.props.trailName === "Cottage Grove Bike Park" ? "Cottage Grove" : this.props.trailName}</b></h2>
+                <h2 className="trail-heading"><b>{resolveAnyLongTrailNames(this.props.trailName)}</b></h2>
                 <h4 className="location-heading">{this.props.trailLocation}</h4>
                 <img className="view-trail-icon" src="https://cdn2.iconfinder.com/data/icons/pittogrammi/142/61-512.png"
                     alt="icon" onClick={(e) => changeMapHandler(e, mapCoordinates[this.props.trailName].reimtbX, mapCoordinates[this.props.trailName].reimtbY)} />
